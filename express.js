6df704263e5169cba1bfdd6e7ec6fd0cd741028a
americanman <snowboardracer7@gmail.com>
2018-08-24 09:47:55 -0400

    Return result from DB

--- express.js
+++ express.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const port = 8900
+// sourced from, https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
+const MongoClient = require('mongodb').MongoClient
 
 let app = express();
 app.get('/', function(req,res){
-  res.json('hello world')
+
+  MongoClient.connect("mongodb://localhost:27017/travel", function(err, db) {
+    console.log('connected successfully to server')
+    db.collection('newtravel').aggregate([ ]).toArray()
+      .then(result => {
+        db.close();
+        res.json(result)
+      })
+  });
 })
 
 app.listen(port);
