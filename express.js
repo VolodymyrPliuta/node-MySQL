890ef891434c248862e3d3641392beadfa637e4b
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-08-24 15:50:54 -0400

    postgresql createDB, insert, drop, and alter functions added.

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ const port = 8900
 // sourced from, https://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
 const MongoClient = require('mongodb').MongoClient
 
+let pg = require('pg');
 let app = express();
 
 app.listen(port);
@@ -45,3 +46,45 @@ function removeSingleRecord(db, count){
   db.collection('newtravel').aggregate([ { $skip: count}, { $limit: 1} ]).toArray()
   .then((result) => { db.collection('newtravel').remove(result[0]) })
 }
+
+app.get('/postgresinsert', function(req, res){
+  let name = 'King Kong';
+  let coordinateArray = [
+    {
+      name: 'King',
+      lat: 54.123456,
+      lng: -89.123456
+    },
+    {
+      name: 'Kong',
+      lat: 54.123456,
+      lng: -89.123456
+    }
+  ]
+  const insertValues = coordinateArray.map(({name, lat, lng}) => {return `(DEFAULT,'${name}', ${lat}, ${lng})`}).join(',')
+  const queryString = `INSERT INTO newtravel VALUES ` + insertValues;
+  console.log(queryString);
+  res.json(queryString)
+
+})
+
+app.get('/postgrescreate', function(req, res){
+
+const queryString = `CREATE TABLE newtravel(
+  ID SERIAL,
+  NAME CHAR(35) NOT NULL,
+  LAT FLOAT NOT NULL,
+  LNG FLOAT NOT NULL);
+`
+  res.json(queryString)
+})
+
+app.get('/postgresdrop', function(req, res){
+  const queryString = `DROP TABLE newtravel;`
+  res.json(queryString)
+})
+
+app.get('/postgresalter', function(req, res){
+  const queryString = ` ALTER TABLE newtravel ALTER COLUMN id`
+  res.json(queryString)
+})
